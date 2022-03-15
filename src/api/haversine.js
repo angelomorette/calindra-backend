917f4a1705b7088336570f01411ed5e33cfb0890
angelomorette <angelomorette@gmail.com>
2022-03-15 20:16:17 -0300

    melhorando a forma de listar as combinacao de endereco

--- src/api/haversine.js
+++ src/api/haversine.js
@@ -6,31 +6,30 @@ module.exports = {
         let listaDistancias = [];
         let count = Object.keys(coordenadas).length
         
+        //calculando a distancia
         for (let l = 0; l < count; l++) {
             for (let c = l + 1; c < count; c++) {
                 
                 const distancia = (haversine(coordenadas[l], coordenadas[c]));
-
+                
                 listaDistancias.push({
                     endereco1: address[l],
                     endereco2: address[c],
-                    distancia: distancia
+                    distancia_km: parseFloat(distancia.toFixed(2))
                 });
             }
         }
+        
+        //Deixando em ordem crescente
+        let listaPorOrdemDeAproximacao = []
 
-        let menorDistancia = listaDistancias[0];
-        let maiorDistancia = listaDistancias[0];
+        listaPorOrdemDeAproximacao = listaDistancias.sort(function(a,b){
+            return a.distancia_km < b.distancia_km ? -1 : a.distancia_km > b.distancia_km ? 1 : 0;
+        });
 
-        for(let i = 1; i < listaDistancias.length; i++){
-            if(listaDistancias[i].distancia < menorDistancia.distancia)
-            {
-                menorDistancia = listaDistancias[i];
-            }
-            if(listaDistancias[i].distancia > maiorDistancia.distancia){
-                maiorDistancia = listaDistancias[i];
-            }
-        }
-        return {menorDistancia, maiorDistancia, listaDistancias};
+        let menorDistancia = listaPorOrdemDeAproximacao[0];
+        let maiorDistancia = listaPorOrdemDeAproximacao[listaPorOrdemDeAproximacao.length -1];
+
+        return { menorDistancia, maiorDistancia, listaPorOrdemDeAproximacao };
     }
 }
